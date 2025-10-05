9c14fb8195dc92bf3e813d276c1c377b21e91971
osamashaikh <shaikhusama745@gmail.com>
2025-10-05 23:39:21 +0530

    initial commit

---
+++ src/MergeSort.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MergeSort {
+
+    public static void merge(int[] arr, int low, int mid, int high) {
+        ArrayList<Integer> temp = new ArrayList<>();
+        int left = low;
+        int right = mid + 1;
+        while (left <= mid && right <= high) {
+            if(arr[left] <= arr[right]){
+                temp.add(arr[left]);
+                left++;
+            }else{
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+        while (left <= mid){
+            temp.add(arr[left]);
+            left++;
+        }
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+        for (int i = low; i <= high; i++){
+            arr[i] = temp.get(i - low);
+        }
+    }
+
+    public static void mergeSort(int[] arr, int low, int high){
+        if(low>=high) return;
+        int mid = (low + high) / 2;
+        mergeSort(arr,low,mid);
+        mergeSort(arr,mid+1,high);
+        merge(arr, low, mid, high);  // merging sorted halves
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 7;
+        int[] arr = {9,4,7,6,3,1,5};
+        System.out.println("Before Sorting Array");
+        for(int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        mergeSort(arr,0,n-1);
+        System.out.println("After sorting array: ");
+        for(int i = 0; i < n;i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
